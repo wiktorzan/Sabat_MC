c5996ef0943237cfc83d86995ed8742283fa8d58
kdulski <kamil.dulski@gmail.com>
2025-03-16 11:34:42 +0100

    correct seeding of the Root enginge

--- main.cc
+++ main.cc
@@ -1,16 +1,26 @@
-#include "G4RunManager.hh"
-#include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
-#include "Randomize.hh"
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include <Randomize.hh>
 
 #include "DetectorConstruction.hh"
-#include "PhysicsList.hh"
 #include "ActionInitialization.hh"
+#include "PhysicsList.hh"
+
+#include <unistd.h>
+#include <chrono>
 
 int main(int argc, char** argv)
 {
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
+
+  int mask = 01001010;
+  using namespace std::chrono;
+  system_clock::time_point now = system_clock::now();
+  long seed = (unsigned int)(system_clock::to_time_t(now)) * 677 * ::getpid();
+  G4Random::setTheSeed(seed);
+
   G4RunManager* runManager = new G4RunManager;
 
   runManager->SetUserInitialization(new DetectorConstruction());
